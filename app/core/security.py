87296cf650bf34b2c80e91549a28da441c370bc9
eliott <eliottvelardeverde5@gmail.com>
2026-05-27 19:13:01 -0500

    test: add integration tests for invalid password, successful login and incorrect password

--- app/core/security.py
+++ app/core/security.py
@@ -29,9 +29,9 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     else:
         expire = datetime.now(timezone.utc) + timedelta(
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )  # noqa: E501
+        )
     to_encode.update({"exp": int(expire.timestamp())})
     encoded_jwt = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
-    )  # noqa: E501
+    )
     return encoded_jwt
--- app/tests/api/test_auth.py
+++ app/tests/api/test_auth.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_register_user_succes(override_session_db):
+async def test_register_user_success(override_session_db):
     response = await override_session_db.post(
         "/api/v1/users/",
         json={
@@ -18,6 +18,25 @@ async def test_register_user_succes(override_session_db):
     assert response.status_code == 201
     data = response.json()
     assert data["email"] == "test@sentinel.com"
+
+
+@pytest.mark.asyncio
+async def test_register_user_fails_with_invalid_password(override_session_db):
+    response = await override_session_db.post(
+        "/api/v1/users/",
+        json={
+            "name": "Test",
+            "last_name": "User",
+            "username": "testuser",
+            "email": "test_invalid@sentinel.com",
+            "phonenumber": "123456789",
+            "password": "superpassword",
+        },
+    )
+
+    assert response.status_code == 422
+    data = response.json()
+    assert "detail" in data
 
 
 @pytest.mark.asyncio
@@ -40,3 +59,34 @@ async def test_registration_fails_when_email_is_already_register(
     assert response.status_code == 400
     data = response.json()
     assert data["detail"] == "Email already registered"
+
+
+@pytest.mark.asyncio
+async def test_login_success(override_session_db, registered_user):
+    response = await override_session_db.post(
+        "/api/v1/auth/login",
+        data={
+            "username": registered_user["email"],
+            "password": registered_user["password"],
+        },
+    )
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["access_token"]
+    assert data["token_type"] == "bearer"
+
+
+@pytest.mark.asyncio
+async def test_login_fails_with_incorrect_password(
+    override_session_db, registered_user
+):
+    response = await override_session_db.post(
+        "/api/v1/auth/login",
+        data={"username": registered_user["email"], "password": "wrong_password123"},
+    )
+
+    assert response.status_code == 401
+    data = response.json()
+    assert "detail" in data
+    assert data["detail"] == "Incorrect username or password"
